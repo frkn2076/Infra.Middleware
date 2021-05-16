6d95c2acacd55f0a6b1a256306f104d0fd74d0d0
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-16 22:03:35 +0300

    Middleware extended for log properties

--- Infra.Middleware/RequestResponseLoggingMiddleware.cs
+++ Infra.Middleware/RequestResponseLoggingMiddleware.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using System;
+using System.Dynamic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Infra.Middleware
@@ -11,15 +13,21 @@ namespace Infra.Middleware
     {
         private readonly RequestDelegate _next;
         private readonly Action<string> _action;
+        private readonly LogProperties _logProperties;
+        private const string _localhostTag = "::1";
+        private const string _localhostName = "localhost";
 
-        public RequestResponseLoggingMiddleware(RequestDelegate next, Action<string> action)
+        public RequestResponseLoggingMiddleware(RequestDelegate next, Action<string> action = null, LogProperties logProperties = null)
         {
             _next = next;
             _action = action;
+            _logProperties = logProperties;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
+            dynamic message = new ExpandoObject();
+
             try
             {
                 context.Request.EnableBuffering();
@@ -32,21 +40,31 @@ namespace Infra.Middleware
                 await _next(context);
                 var responseBody = await GetResponseBody(context.Response);
 
-                var message = new
-                {
-                    Endpoint = context.Request.Path,
-                    IPAddress = context.Connection.RemoteIpAddress.ToString(),
-                    RequestHeaders = context.Request.Headers,
-                    RequestBody = requestBody,
-                    ResponseHeaders = context.Response.Headers,
-                    ResponseBody = responseBody,
-                    ResponseStatusCode = context.Response.StatusCode,
-                    Time = DateTime.Now
-                };
+                if (_logProperties?.HasRequestPath != false)
+                    message.RequestPath = context.Request.Path;
+
+                if (_logProperties?.HasRemoteIpAddress != false)
+                    message.RemoteIpAddress = context.Connection.RemoteIpAddress.ToString() == _localhostTag
+                        ? _localhostName : context.Connection.RemoteIpAddress.ToString();
+
+                if (_logProperties?.HasRequestHeaders != false)
+                    message.RequestHeaders = context.Request.Headers;
+
+                if (_logProperties?.HasRequestBody != false)
+                    message.RequestBody = requestBody;
+
+                if (_logProperties?.HasResponseStatusCode != false)
+                    message.ResponseStatusCode = context.Response.StatusCode;
+
+                if (_logProperties?.HasResponseBody != false)
+                    message.ResponseBody = responseBody;
+
+                if (_logProperties?.HasCreatedAt != false)
+                    message.CreatedAt = DateTime.UtcNow;
 
                 var logRecord = JsonConvert.SerializeObject(message);
 
-                _action(logRecord);
+                (_action ?? Console.WriteLine)(logRecord);
 
                 await responseBodyStream.CopyToAsync(originalBodyStream);
             }
@@ -54,36 +72,51 @@ namespace Infra.Middleware
             {
                 context.Request.EnableBuffering();
                 var requestBody = await GetRequestBody(context.Request);
-                var message = new
-                {
-                    Endpoint = context.Request.Path,
-                    IPAddress = context.Connection.RemoteIpAddress.ToString(),
-                    RequestHeaders = context.Request.Headers,
-                    RequestBody = requestBody,
-                    ResponseBody = ex.Message,
-                    Time = DateTime.Now
-                };
+
+                if (_logProperties?.HasRequestPath != false)
+                    message.RequestPath = context.Request.Path;
+
+                if (_logProperties?.HasRemoteIpAddress != false)
+                    message.RemoteIpAddress = context.Connection.RemoteIpAddress.ToString() == _localhostTag
+                        ? _localhostName : context.Connection.RemoteIpAddress.ToString();
+
+                if (_logProperties?.HasRequestHeaders != false)
+                    message.RequestHeaders = context.Request.Headers;
+
+                if (_logProperties?.HasRequestBody != false)
+                    message.RequestBody = requestBody;
+
+                if (_logProperties?.HasResponseStatusCode != false)
+                    message.ResponseStatusCode = null;
+
+                if (_logProperties?.HasResponseBody != false)
+                    message.ResponseBody = ex.Message;
+
+                if (_logProperties?.HasCreatedAt != false)
+                    message.CreatedAt = DateTime.UtcNow;
 
                 var logRecord = JsonConvert.SerializeObject(message);
 
-                _action(logRecord);
+                (_action ?? Console.WriteLine)(logRecord);
 
                 throw;
             }
         }
 
-        private async Task<string> GetRequestBody(HttpRequest request)
+        private async Task<dynamic> GetRequestBody(HttpRequest request)
         {
             using var reader = new StreamReader(request.Body, encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false, leaveOpen: true);
-            var body = await reader.ReadToEndAsync();
+            var requestBody = await reader.ReadToEndAsync();
+            var body = JsonConvert.DeserializeObject<dynamic>(requestBody);
             request.Body.Position = 0;
             return body;
         }
 
-        private async Task<string> GetResponseBody(HttpResponse response)
+        private async Task<dynamic> GetResponseBody(HttpResponse response)
         {
             response.Body.Seek(0, SeekOrigin.Begin);
-            string body = await new StreamReader(response.Body).ReadToEndAsync();
+            var responseBody = await new StreamReader(response.Body).ReadToEndAsync();
+            var body = JsonConvert.DeserializeObject<dynamic>(responseBody);
             response.Body.Seek(0, SeekOrigin.Begin);
             return body;
         }
