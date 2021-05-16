3ca829ee9012c67849b894a23126968953b8cf39
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-16 22:02:19 +0300

    Log properties made as choosable

---
+++ Infra.Middleware/LogProperties.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Infra.Middleware
+{
+    public class LogProperties
+    {
+        [DefaultValue(true)]
+        public bool HasRequestPath { get; set; }
+        [DefaultValue(false)]
+        public bool HasRemoteIpAddress { get; set; }
+        [DefaultValue(false)]
+        public bool HasRequestHeaders { get; set; }
+        [DefaultValue(false)]
+        public bool HasRequestBody { get; set; }
+        [DefaultValue(false)]
+        public bool HasResponseStatusCode { get; set; }
+        [DefaultValue(false)]
+        public bool HasResponseBody { get; set; }
+        [DefaultValue(false)]
+        public bool HasCreatedAt { get; set; }
+    }
+}
